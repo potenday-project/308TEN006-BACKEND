8c412241fb8fbb38fbc79b45472c60fd9a2d4f32
integerJI <jjs9536@gmail.com>
2023-08-06 14:53:48 +0900

    Feat: 회원가입 기능 추가

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-import board.views
+from django.urls import path, include
+from django.conf import settings
+import board.views, member.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', board.views.index, name='index'),
+    path('member/', include('member.urls')),
 ]
