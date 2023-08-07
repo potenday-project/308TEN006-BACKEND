97d3e03314a56826c516fac967503dd8f4adb9d0
integerJI <jjs9536@gmail.com>
2023-08-07 23:30:00 +0900

    Feat: front add

--- main/settings.py
+++ main/settings.py
@@ -136,7 +136,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# static, media file setting
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'board', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
