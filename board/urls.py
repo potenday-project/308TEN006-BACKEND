0b1deaafbaa697504de28efc3fd3c440c730cefa
integerJI <jjs9536@gmail.com>
2023-08-12 12:42:33 +0900

    Feat: 카테고리 추가

--- board/urls.py
+++ board/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('<int:memokey>/comment_write/', views.comment_write, name='comment_write'),
     path('<int:memo_pk>/<int:pk>/comment_delete/', views.comment_delete, name='comment_delete'),
     path('category/<str:category_name>/', views.category, name='category'),
-
+    path('explore/tags/<str:tag>/', views.index, name='post_search'),
+    
     ### 하나씩 살리기 ~ ###
     # path('<int:memokey>/modify/', views.modify, name='modify_memo'),
     # path('<int:memokey>/delete/', views.delete, name='delete_memo'),
-    path('explore/tags/<str:tag>/', views.index, name='post_search'),
     # path('search/', views.search, name='search'),
     ###
 ]
